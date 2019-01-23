221dfd6ffb6ca164f4b3a36d45e79a227d659630
Sumeet Pol <polsumeet3666@gmail.com>
2019-01-23 10:24:03 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import head from './0.png';
+import tail from './1.png';
 import './App.css';
 
 class App extends Component {
+
+	constructor(){
+		super();
+		this.state= {
+			result:null,
+			flip:false
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(){
+		//alert('clicked');
+
+		this.setState({...this.setState,flip:true});
+		setTimeout(()=>{
+			var result = Math.floor(Math.random()*Math.floor(2));
+			this.setState({result,flip:false});
+		},3000);
+		
+	}
+
   render() {
+  	let resultText;
+  	let resultImg;
+  	let fliping;
+    	if(this.state.result != null){
+         	resultText = <h2 id="result">{this.state.result ==0 ?"Heads":"Tails"}</h2>	
+         	resultImg= <img src={this.state.result ==0 ? head:tail} />
+         }
+
+         if(this.state.flip){
+         	fliping = <h2>Fliping ...</h2>
+         	resultImg=null;
+         	resultText=null;
+         }
+         else {
+         	fliping = "";
+         }
     return (
+    	
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header" >
+         <h1> Coin Flip Simulation</h1>
+         <div >
+         	<br/>
+         {resultImg}
+         <br/>
+         <br/>
+         {resultText}
+         <br/>
+         {fliping}
+         </div>
+         
+         
+         <button type="button" class="btn btn-success" onClick={this.handleClick}>Flip Coin</button>
         </header>
       </div>
     );
